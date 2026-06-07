2a8e3a18a3327fa47efa985f1ba328f0460c55a0
mohsinazmi90 <mohsin.azmi1990@gmail.com>
2026-06-07 21:54:30 +0400

    added files and text.py files

---
+++ test.py
@@ -0,0 +1,12 @@
+numbers = [100, 200, 300, 400, 500]
+
+reverse_numbers = numbers[::-1]
+print(reverse_numbers)
+
+# Starter code
+items = [1,2,3,4,5]
+try:
+    item = items[6]
+except Exception as e:
+    print(e)
+print(item)
