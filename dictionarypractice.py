193351a9fdc830d2b867673820f1348399157295
mohsinazmi90 <mohsin.azmi1990@gmail.com>
2026-06-15 23:58:17 +0400

    Inital Commit. Dictionary practice

---
+++ dictionarypractice.py
@@ -0,0 +1,25 @@
+# Q4. Dictionaries
+# Create a dictionary to store the following student information:
+# •     Name
+# •     Roll Number
+# •     Class
+# •     Marks
+# Then display all key–value pairs in the dictionary.
+
+def main():
+    
+    students = {
+        1: {"Name": "Mohsin", "Roll": 1, "Class": "Math", "Marks": 90},
+        2: {"Name": "Sara", "Roll": 2, "Class": "Science", "Marks": 80},
+        3: {"Name": "Zara", "Roll": 3, "Class": "Arts", "Marks": 92},
+        4: {"Name": "Zaed", "Roll": 4, "Class": "English", "Marks": 96},
+    }
+    
+    print(f"{'Name':<10} {'Roll':<6} {'Class':<10} {'Marks':<6}") 
+    print('-' * 35)
+    
+    for roll, student in students.items():
+        print(f"{student['Name']:<10} {roll:<6} {student['Class']:<10} {student['Marks']:<6}")
+    
+if __name__ == "__main__":
+    main()
